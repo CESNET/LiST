a99bd708f0e83afe8ecaa389f09ac977e5693239
Tomas Cejka <cejkat@cesnet.cz>
2019-08-12 16:51:50 +0200

    warden-server: register local warden-client for demo

--- ansible/roles/warden-server/files/install/generate-warden-cfg.sh
+++ ansible/roles/warden-server/files/install/generate-warden-cfg.sh
@@ -2,5 +2,11 @@
 
 HOSTNAME=`hostname -f`
 mkdir /etc/warden
+
+# test if the name is not registered
+/opt/warden_server_3/warden_server.py list | grep -q com.example.list_vagrant.nemea.filer;
+if [ $? -ne 0 ]; then
+# register local warden client
 python3 /opt/warden_server_3/warden_server.py register -n com.example.list_vagrant.nemea.filer -h "$HOSTNAME" -r list@example.com --write --valid --notest
+fi
 
